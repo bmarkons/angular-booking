11f020dd3def17c21cbb96fb96ea39fcb1396b22
bmarkons <mamaveb@gmail.com>
2017-08-30 20:20:46 +0200

    Setup angular materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { Angular2TokenService } from 'angular2-token';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     HttpModule,
     RouterModule
+    MaterializeModule
   ],
   providers: [ Angular2TokenService ],
   bootstrap: [ AppComponent ]
