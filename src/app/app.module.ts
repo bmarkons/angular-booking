1e1aab088ebda3f7076a5aae9746cf8ceb2f73fa
bmarkons <mamaveb@gmail.com>
2017-08-30 19:15:18 +0200

    Introduce angular2-token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { Angular2TokenService } from 'angular2-token';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ Angular2TokenService ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
